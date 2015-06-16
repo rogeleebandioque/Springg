30ec2cd56620b8857ecf930cd1bdaf44c4d48a68
Rogelee <rogelee.bandioque@gmail.com>
2015-06-16 17:35:23 +0800

    Revert "list and delete"
    This reverts commit e984d02777ebfdcc1d538fd2417eebe51fb5c26b.

--- hibernateactivity-core/hibernateactivity-core-dao/src/main/java/hibernateactivity/core/dao/PersonDao.java
+++
@@ -1,9 +0,0 @@
-package hibernateactivity.core.dao;
-import java.util.*;
-import hibernateactivity.core.model.Person;
-
-interface PersonDao{
-    public List<Person> getPeople();
-    public String deletePeople(int idNum);
-   
-}

89e611dee7d7b7c5fa759bdcbf9ccaff00427688
Rogelee <rogelee.bandioque@gmail.com>
2015-06-16 09:19:46 +0800

    modified folder names

---
+++ hibernateactivity-core/hibernateactivity-core-dao/src/main/java/hibernateactivity/core/dao/App.java
@@ -0,0 +1,13 @@
+package hibernateactivity.core.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

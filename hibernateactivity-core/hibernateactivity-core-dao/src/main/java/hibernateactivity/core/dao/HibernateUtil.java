b5a53811110b3823b4c8bf6ae0553a0ec03013f9
Rogelee <rogelee.bandioque@gmail.com>
2015-07-21 11:33:36 +0800

    xml based

--- hibernateactivity-core/hibernateactivity-core-dao/src/main/java/hibernateactivity/core/dao/HibernateUtil.java
+++ hibernateactivity-core/hibernateactivity-core-dao/src/main/java/hibernateactivity/core/dao/HibernateUtil.java
@@ -13,27 +13,11 @@ public class HibernateUtil {
     private static Session session;
     private static Transaction transaction;
     
-//    public HibernateUtil(SessionFactory sessionFactory){
-//        this.sessionFactory = sessionFactory;
-//    }
     public void setSessionFactory(SessionFactory sessionFactory){
         this.sessionFactory = sessionFactory;
     }
-    /*  public static void init() {
-       try {
-            factory = new AnnotationConfiguration().configure("hibernate.cfg.xml").buildSessionFactory(); 
-        } catch (Throwable e) {
-            System.err.println("Failed to create session factory object.");
-            e.printStackTrace();
-        }
-            
-        
-    }*/
     
     public static <T> T perform(Command command, Class<T> returnClass) {
-        /*if(session == null) {
-            init();
-        }*/
         session = sessionFactory.openSession();
         transaction = session.beginTransaction();
         
@@ -55,6 +39,4 @@ public class HibernateUtil {
         
         return returnClass.cast(returnObject);
     }
-
-
 }
